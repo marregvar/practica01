0879a8f3a7e65621d219924d3f480f43fde86e69
marregvar <mregueravarela@yahoo.es>
2016-11-20 23:40:42 +0100

    Add files via upload

---
+++ src/main/MyClass.java
@@ -0,0 +1,15 @@
+public class MyClass{
+  public MyClass(){};
+  
+  public void muestraMensaje(String msg){
+    System.out.println(msg);
+  }
+  
+  public String devuelveMensaje(){
+    return "Hello World!";
+  }
+  
+  public int suma(int a, int b){
+    return a+b;
+  }
+}
